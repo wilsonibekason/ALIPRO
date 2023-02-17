0a893e354058d0ae134f8777914832c555efc2cd
wilsonibekason <wanbekgithub@gmail>
2023-02-17 13:01:11 +0100

    add welcome image section

--- App.js
+++ App.js
@@ -21,11 +21,13 @@ import {
   useDeviceOrientation,
 } from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
     <>
-      <WelcomeScreen />
+      {/* <WelcomeScreen /> */}
+      <ViewImageScreen />
     </>
   );
 }
