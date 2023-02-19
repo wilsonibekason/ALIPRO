7cee195812c35f13b12c6e0b11963fceee91ae4a
wilsonibekason <wanbekgithub@gmail.com>
2023-02-19 17:54:55 +0100

    Add codesnipet

---
+++ app/components/Test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const Test = ({}) => {
+  return (
+    <>
+      <View></View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Test;
