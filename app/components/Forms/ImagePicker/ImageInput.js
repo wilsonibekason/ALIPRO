d315b0159c42d4f8801c29b7d0d9d9a77dddcf15
wilsonibekason <wanbekgithub@gmail.com>
2023-02-20 15:41:04 +0100

    add image select function

---
+++ app/components/Forms/ImagePicker/ImageInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import colors from "../../../config/colors";
+
+const ImageInput = ({}) => {
+  return (
+    <>
+      <View></View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.lightGray,
+    alignItems: "center",
+    borderRadius: 15,
+    height: 100,
+    justifyContent: "center",
+    width: 100,
+  },
+});
+
+export default ImageInput;
